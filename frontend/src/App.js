770c42b9859493f092ccbb872effedfd1a3f12fb
Cobas91 <stephan.gaertner72@outlook.de>
2021-10-08 13:12:40 +0200

    added non styled html and prettify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,95 @@
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <image name="Logo" alt="logo" />
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#home">Add Question</a>
+          </li>
+        </ul>
       </header>
+      <body>
+        <section>
+          <div className="question">
+            <p className="question-text"> Your Question here!</p>
+            <div className="answers">
+              <ul>
+                <li>Answer 1</li>
+                <li>Answer 2</li>
+                <li>Answer 3</li>
+              </ul>
+            </div>
+          </div>
+          <div className="question">
+            <p className="question-text"> Your Question here!</p>
+            <div className="answers">
+              <ul>
+                <li>Answer 1</li>
+                <li>Answer 2</li>
+                <li>Answer 3</li>
+              </ul>
+            </div>
+          </div>
+          <div className="question">
+            <p className="question-text"> Your Question here!</p>
+            <div className="answers">
+              <ul>
+                <li>Answer 1</li>
+                <li>Answer 2</li>
+                <li>Answer 3</li>
+              </ul>
+            </div>
+          </div>
+          <div className="question">
+            <p className="question-text"> Your Question here!</p>
+            <div className="answers">
+              <ul>
+                <li>Answer 1</li>
+                <li>Answer 2</li>
+                <li>Answer 3</li>
+              </ul>
+            </div>
+          </div>
+          <div className="question">
+            <p className="question-text"> Your Question here!</p>
+            <div className="answers">
+              <ul>
+                <li>Answer 1</li>
+                <li>Answer 2</li>
+                <li>Answer 3</li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </body>
+      <hr />
+      <h2>Add Question Page</h2>
+      <body>
+        <form className="question">
+          <textarea placeholder="Insert your Question here...." />
+          <section>
+            <div className="answer-1">
+              <p>Answer 1</p>
+              <input className="answer-1-text" />
+              <p>Is Correct?</p>
+              <input type="radio" className="isCorrect" />
+            </div>
+            <div className="answer-2">
+              <p>Answer 2</p>
+              <input className="answer-2-text" />
+              <p>Is Correct?</p>
+              <input type="radio" className="isCorrect" />
+            </div>
+            <button type="button">New answer</button>
+          </section>
+        </form>
+      </body>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
