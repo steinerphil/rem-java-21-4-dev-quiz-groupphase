5854bd242e22ab6ea691aadc0d5c9af7a442d46f
Laifson <marvinlaif@live.de>
2021-10-10 19:33:37 +0200

    Add tests for listQuestions(), get() and addQuestion() Methods

--- backend/src/test/java/de/neuefische/devquiz/repo/QuestionRepoTest.java
+++ backend/src/test/java/de/neuefische/devquiz/repo/QuestionRepoTest.java
@@ -1,70 +1,68 @@
 package de.neuefische.devquiz.repo;
 
-import de.neuefische.devquiz.model.Answer;
 import de.neuefische.devquiz.model.Question;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class QuestionRepoTest {
 
     @Test
     void listQuestions() {
         // GIVEN
-        List<Answer> answerList = List.of(
-                new Answer("1", "Hallo", true),
-                new Answer("2", "Hey", false),
-                new Answer("3", "Hey123123", false),
-                new Answer("4", "Hey3213232", false)
+        QuestionRepo questionRepo = new QuestionRepo();
+        questionRepo.addQuestion(new Question("1", "Whats the most beautiful lombock?", null));
+        questionRepo.addQuestion(new Question("2", "Whats the most beautiful dog?", null));
+        questionRepo.addQuestion(new Question("3", "Whats the most beautiful hamcrest?", null));
+        List<Question> actual = List.of(
+                new Question("1", "Whats the most beautiful lombock?", null),
+                new Question("2", "Whats the most beautiful dog?", null),
+                new Question("3", "Whats the most beautiful hamcrest?", null)
         );
 
-        QuestionRepo questionRepo = new QuestionRepo();
-
-        questionRepo.addQuestion(
-                new Question("1", "Wie sagt man am besten guten Tag?", answerList));
-
         // WHEN
-        String expected = "[Question(id=1, questionText=Wie sagt man am besten guten Tag?, answers=[Answer(id=1, answerText=Hallo, isCorrect=true), Answer(id=2, answerText=Hey, isCorrect=false), Answer(id=3, answerText=Hey123123, isCorrect=false), Answer(id=4, answerText=Hey3213232, isCorrect=false)])]";
+        List<Question> expected = questionRepo.listQuestions();
 
         // THEN
-        Assertions.assertEquals(expected, questionRepo.listQuestions());
+        assertEquals(expected, actual);
 
     }
 
     @Test
     void get() {
         // GIVEN
-        List<Answer> answerList = List.of(
-                new Answer("1", "Hallo", true),
-                new Answer("2", "Hey", false),
-                new Answer("3", "Hey123123", false),
-                new Answer("4", "Hey3213232", false)
-        );
-
         QuestionRepo questionRepo = new QuestionRepo();
-
-        List<Question> questionList = List.of(
-                new Question("1", "Wie sagt man am besten guten Tag?", answerList),
-                new Question("2", "Was ist das?", answerList),
-                new Question("3", "Question 1238?", answerList)
-        );
+        questionRepo.addQuestion(new Question("1", "Wie sagt man am besten guten Tag?", null));
+        questionRepo.addQuestion(new Question("2", "Was ist das?", null));
+        questionRepo.addQuestion(new Question("3", "Question 1238?", null));
 
         // WHEN
-        String excepted = "Question(id=2, questionText=Was ist das?, answers=[Answer(id=1, answerText=Hallo, isCorrect=true), Answer(id=2, answerText=Hey, isCorrect=false), Answer(id=3, answerText=Hey123123, isCorrect=false), Answer(id=4, answerText=Hey3213232, isCorrect=false)])";
+        Question expected1 = new Question("1", "Wie sagt man am besten guten Tag?", null);
+        Question expected2 = new Question("2", "Was ist das?", null);
+        Question expected3 = new Question("3", "Question 1238?", null);
 
 
         // THEN
-        Assertions.assertEquals(excepted, questionList.get(1));
+        assertEquals(expected1, questionRepo.get("1"));
+        assertEquals(expected2, questionRepo.get("2"));
+        assertEquals(expected3, questionRepo.get("3"));
 
 
     }
 
-
     @Test
     void addQuestion() {
+        // GIVEN
+        Question questionToAdd = new Question("4", "Hallo", null);
+        QuestionRepo questionRepo = new QuestionRepo();
+
+        // WHEN
+        questionRepo.addQuestion(questionToAdd);
+
+        //THEN
+        assertEquals(questionRepo.get("4"), questionToAdd);
     }
 }
