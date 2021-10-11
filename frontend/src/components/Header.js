da56d8219a0e4f3c62f4e65f8f7e396d37d970e7
Cobas91 <stephan.gaertner72@outlook.de>
2021-10-11 11:27:04 +0200

    style navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,24 @@
-import {HiAcademicCap} from "react-icons/hi";
-import Navbar from "./Navbar";
+import { HiAcademicCap } from 'react-icons/hi'
+import Navbar from './Navbar'
+import styled from 'styled-components'
 
-function Header(){
-    return(
-        <header>
-            <HiAcademicCap/>
-            <Navbar/>
-        </header>
-    )
-}export default Header
+function Header() {
+  return (
+    <HeaderContainer>
+      <Logo />
+      <Navbar />
+    </HeaderContainer>
+  )
+}
+export default Header
+
+const HeaderContainer = styled.section`
+  display: grid;
+  grid-template-columns: min-content 1fr;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const Logo = styled(HiAcademicCap)`
+  font-size: 100px;
+`
