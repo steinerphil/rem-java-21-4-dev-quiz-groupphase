fffd97715cbad9a1156ffc232864ebb0b5a8a8ad
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-10-12 15:33:33 +0200

    Change export in devQuizApiService.js

--- frontend/src/service/devQuizApiService.js
+++ frontend/src/service/devQuizApiService.js
@@ -1,16 +1,15 @@
 import axios from 'axios'
 
-function getQuestions() {
+export function getQuestions() {
   return axios
     .get('/api/question')
     .then(response => response.data)
     .catch(err => console.error(err))
 }
 
-function addQuestion(newQuestion) {
+export function addQuestion(newQuestion) {
   return axios
     .post('/api/question', newQuestion)
     .then(response => response.data)
     .catch(console.error)
 }
-export { getQuestions, addQuestion }
