fc237613c8798876050ba32da2a5935b382005e1
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-10-12 14:21:06 +0200

    Add RequestMapping Annotation to DevQuizController

--- backend/src/main/java/de/neuefische/devquiz/controller/DevQuizController.java
+++ backend/src/main/java/de/neuefische/devquiz/controller/DevQuizController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/question")
 public class DevQuizController {
 
     private final QuestionService questionService;
@@ -18,17 +19,17 @@ public class DevQuizController {
         this.questionService = questionService;
     }
 
-    @GetMapping("/api/question")
+    @GetMapping()
     public List<Question> listQuestion() {
         return questionService.getAllQuestions();
     }
 
-    @GetMapping("/api/question/{id}")
+    @GetMapping("/{id}")
     public Question get(@PathVariable String id) {
         return questionService.get(id);
     }
 
-    @PostMapping("/api/question")
+    @PostMapping()
     public Question addQuestion(@RequestBody Question newQuestion) {
         return questionService.addQuestion(newQuestion);
     }
