d2073c7e85cca21122d4d9c0d06dbd804a53676a
Philipp Steiner <philipp-steiner@gmx.de>
2021-10-13 17:08:39 +0200

    refactor add question method, generate uuids

--- backend/src/main/java/de/neuefische/devquiz/service/QuestionService.java
+++ backend/src/main/java/de/neuefische/devquiz/service/QuestionService.java
@@ -1,10 +1,11 @@
 package de.neuefische.devquiz.service;
 
+import de.neuefische.devquiz.model.Answer;
 import de.neuefische.devquiz.model.Question;
 import de.neuefische.devquiz.repo.QuestionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import de.neuefische.devquiz.service.IdService;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -13,6 +14,7 @@ import java.util.Optional;
 public class QuestionService {
 
     private final QuestionRepo questionRepo;
+    private final IdService idService = new IdService();
 
     @Autowired
     public QuestionService(QuestionRepo questionRepo) {
@@ -24,6 +26,14 @@ public class QuestionService {
     }
 
     public Question addQuestion(Question newQuestion){
+            //TODO Return List of Questions
+            //TODO Set Answer ID´s before saving the question
+
+            newQuestion.setId(idService.generateId());
+
+            for (Answer answer : newQuestion.getAnswers()) {
+                answer.setId(idService.generateId());
+            }
 
         return questionRepo.save(newQuestion);
     }
